dbfbff5c6572c6e9cc9f0260dbfc9445ac7d046c
BasilMousa <74674333+BasilMousa@users.noreply.github.com>
2025-03-21 09:36:46 +0200

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,34 @@
+def addIncome(budgetData) 
+
+
+return budgetData
+
+
+
+
+def addExpense(budgetData)
+
+
+
+return budgetData
+
+
+
+
+
+
+def showBalance(budgetData)
+
+
+
+return budgetData
+
+
+
+
+
+def showTransactionHistory(budgetData)
+
+
+
+return budgetData
