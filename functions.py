b5fa409466b7ae18e8bd2148020bccdf1284e6fb
anern13 <anern13@gmail.com>
2025-03-21 11:23:33 +0200

    implimented functionality

--- functions.py
+++ functions.py
@@ -1,34 +1,66 @@
-def addIncome(budgetData) 
+balance = 0
 
+def addIncome(budgetData) :
+    global balance
+    amount = input("enter an amount")
+    desc = input("enter a description")
 
-return budgetData
+    row = ("income", amount, desc)
+    budgetData.append(row)
+    balance += amount
 
+    while True:
+        choice = input("Do you want to add another ?(Y/n)")
+        match choice:
+            case "Y":
+                return addIncome(budgetData)
+            case "y":
+                return addIncome(budgetData)
+            case "n":
+                break
+            case _:
+                print("invalid input")
 
-
-
-def addExpense(budgetData)
-
-
-
-return budgetData
-
-
-
-
-
-
-def showBalance(budgetData)
-
-
-
-return budgetData
-
-
-
-
-
-def showTransactionHistory(budgetData)
+    return budgetData
 
 
 
-return budgetData
+
+def addExpense(budgetData):
+    global balance
+    amount = input("enter an amount")
+    desc = input("enter a description")
+
+    row = ("expense", amount, desc)
+    budgetData.append(row)
+    balance -= amount
+
+    while True:
+        choice = input("Do you want to add another ?(Y/n)")
+        match choice:
+            case "Y":
+                return addIncome(budgetData)
+            case "y":
+                return addIncome(budgetData)
+            case "n":
+                break
+            case _:
+                print("invalid input")
+    return budgetData
+
+
+
+
+
+
+def showBalance(budgetData):
+    print(f"Your current balance is {balance}")
+    return budgetData
+
+
+
+
+
+def showTransactionHistory(budgetData):
+    print(f"Your recent transactions: {budgetData}")
+    return budgetData
